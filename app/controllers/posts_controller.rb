2cfc5cb7a93180c62b087736902474d1248ea490
AbikoHisashi <igtpiyr@docomo.ne.jp>
2020-05-02 03:48:12 +0900

    added  edit and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,28 @@ class PostsController < ApplicationController
     if post.save
       redirect_to("/posts/index")
     else
-      render("/posts/new")
+      render("/posts/edit")
     end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id] )
+  end
+
+  def update
+    post = Post.find_by(id: params[:id] )
+    post.content = params[:content]
+    post.id = params[:id]
+    if post.save
+      redirect_to("/posts/index", {method:post})
+    else
+      redirect_to("/posts/index", {method:post})
+    end
+  end
+  
+  def destroy
+    post = Post.find_by(id: params[:id] )
+    post.destroy
+    redirect_to("/posts/index")
   end
 end
